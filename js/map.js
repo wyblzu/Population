0b923f432b0ca6f11b1388da9ff2e9992819a430
wyblzu <1026809921@qq.com>
2017-03-13 18:59:16 +0800

    加载谷歌离线瓦片已完成！

--- js/map.js
+++ js/map.js
@@ -13,6 +13,7 @@ function initMap() {
     var vec_c = getTdtLayer("vec_c");
     var cva_c = getTdtLayer("cva_c");
     var heatMapLayer = createHeatMap();
+    var imageTianDiTu = loadImageTile();
     var arcgisCacheLayer = loadArcGISCacheLayer();
     var layerGroup = new ol.layer.Group({
         title: '天地图',
@@ -33,7 +34,7 @@ function initMap() {
             }),
             new ol.layer.Group({
                 title: '热力图',
-                layers: [heatMapLayer, arcgisCacheLayer, loadImageTile()]
+                layers: [heatMapLayer, arcgisCacheLayer, imageTianDiTu]
             })
         ],
         view : new ol.View({
@@ -89,6 +90,40 @@ function getTdtLayer(lyr) {
 }
 function createHeatMap() {
     var heatmapSource = new ol.source.Vector();
+    // fetch('data/cluster.json').then(function (response) {
+    //     return response.json();
+    // }).then(function (json) {
+    //     var maxCountValue = maxCount(json);
+    //     var featureJson = [];
+    //     $.each(json, function (index, info) {
+    //         var quadkey = info['gridkey'];
+    //         var coods = quadgrid_decode(quadkey);
+    //         var geometry = new ol.geom.Polygon(null);
+    //         //构造几何
+    //         geometry.setCoordinates([[[coods[0], coods[3]],[coods[2], coods[3]], [coods[2], coods[1]],[coods[0],coods[1]], [coods[0], coods[3]]]]);
+    //         //求取中心点
+    //         var center = new ol.extent.getCenter(geometry.getExtent());
+    //         var weight = info['count']/maxCountValue;
+    //         var interJson = {
+    //             'type': 'Feature',
+    //             'geometry': {
+    //                 'type': 'Point',
+    //                 'coordinates': center
+    //             },
+    //             'properties': {
+    //                 'weight': weight
+    //             }
+    //         };
+    //         featureJson.push(interJson);
+    //     });
+    //     var heatmapJSON = {
+    //         'type': 'FeatureCollection',
+    //         'features': featureJson
+    //     };
+    //     heatmapSource = new ol.source.Vector({
+    //         features: (new ol.format.GeoJSON()).readFeatures(JSON.stringify(heatmapJSON))
+    //     });
+    // });
     //同步请求
     $.ajaxSettings.async = false;
     $.getJSON('data/cluster.json', function (data) {
@@ -176,6 +211,8 @@ function loadImageTile() {
     }
     var tileOrigin = ol.extent.getTopLeft(projectionExtent);
     var layer = new ol.layer.Tile({
+        extent: [ -180, -90, 180, 90],
+        preload:0,
         source: new ol.source.XYZ({
             maxZoom: 18,
             minZoom:1,
@@ -188,6 +225,7 @@ function loadImageTile() {
                 if (x * n < 0) {
                     x = x + n;
                 }
+                // return 'http://localhost:5555/TilesService/servlet/TilesRequest?z=' + z + '&x=' + x + '&y='+ y;
                 return './data/tile/' + z + '/' + x + '/' + y + '.png';
             },
             projection: projection,
@@ -261,9 +299,68 @@ function findBy(layer, key, value) {
     }
     return null;
 }
-initMap();
-
-
+//加载谷歌缓存瓦片
+function loadGoogleTilesCache() {
+    var projectionExtent = [ -20037508.3427892, -20037508.3427892, 20037508.3427892, 20037508.3427892 ];
+    var tileOrigin = ol.extent.getTopLeft(projectionExtent);
+    var maxResolution = ol.extent.getWidth(projectionExtent) / 256;
+    var resolutions = new Array(19);
+    for(var i = 0; i < 19; i ++){
+        resolutions[i] = maxResolution/ Math.pow(2, i);
+    }
+    var layer = new ol.layer.Tile({
+        source: new ol.source.XYZ({
+            maxZoom: 18,
+            minZoom:0,
+            tileUrlFunction: function (tileCoord) {
+                var z = tileCoord[0];
+                var x = tileCoord[1];
+                var y = -tileCoord[2]-1;
+                return './data/googletile/' + z + '/' + x + '/' + y + '.png';
+            },
+            projection: 'EPSG:900913',
+            tileGrid: new ol.tilegrid.TileGrid({
+                origin: tileOrigin,
+                extent: projectionExtent,
+                resolutions: resolutions,
+                tileSize: [256, 256]
+            }),
+            wrapX: false
+        })
+    });
+    var googleMapLayer = new ol.layer.Tile({
+        source: new ol.source.XYZ({
+            url:'http://www.google.cn/maps/vt/pb=!1m4!1m3!1i{z}!2i{x}!3i{y}!2m3!1e0!2sm!3i345013117!3m8!2szh-CN!3scn!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0'
+        })
+    });
+    var map = new ol.Map({
+        controls: ol.control.defaults({
+            attribution: false
+        }),
+        target: 'map',
+        view : new ol.View({
+            projection: 'EPSG:900913',
+            center: ol.proj.transform([116.3, 39.9], "EPSG:4326", 'EPSG:900913'),
+            minZoom: 0,
+            maxZoom: 18,
+            zoom: 5
+        })
+    });
+    map.addLayer(layer);
+    // map.addLayer(googleMapLayer);
+    showMousePosition(map);
+}
+//显示鼠标位置
+function showMousePosition(map) {
+    var element = document.getElementById('mousePosition');
+    map.on("pointermove", function (event) {
+        var coord = event.coordinate;
+        var formatCoord = ol.coordinate.format(ol.proj.transform(coord, 'EPSG:900913', 'EPSG:4326'), '{x},{y}', 4);
+        element.innerHTML = formatCoord;
+    })
+}
+// initMap();
+loadGoogleTilesCache();
 
 
 
